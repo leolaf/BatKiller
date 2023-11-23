b77045469d09b50be7c0eea3d2b4dca1c81c8444
Léo Laffont <leo.laffont@etu.univ-smb.fr>
2023-11-23 03:10:35 +0100

    Bats

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField]
+    private float speed = 500;
+
+    [SerializeField]
+    private float jumpForce = 500;
+
+    [SerializeField]
+    private float gravity = 10;
+
+    private float horizontalMovement = 0;
+    private float verticalMovement = 0;
+
+    private bool onGround = false;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity = Vector3.down * gravity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = 0;
+        if (onGround)
+        {
+            verticalMovement = Input.GetAxis("Jump");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + new Vector3(horizontalMovement, 0, 0) * speed * Time.fixedDeltaTime);
+        rb.velocity += new Vector3(0, verticalMovement, 0) * jumpForce;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Terrain")
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Terrain")
+        {
+            onGround = false;
+        }
     }
 }
